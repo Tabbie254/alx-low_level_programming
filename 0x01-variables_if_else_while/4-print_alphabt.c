31cd350dc32f6887d2b249f0f2acfb5c211da5c1
Tabbie254 <tabbie440@gmail.com>
2022-06-03 07:13:39 -0700

    progam that print all lowercase alphabets excepr q and e then a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#includ <stdio.h>
+
+/**
+ * main -text input
+ *
+ * Return: always 0 (if success)
+ */
+int main(void)
+{
+	char c ='a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
