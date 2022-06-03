eed8e03a5deea5f9a438c8fd764d4d3aa33c8b9f
Tabbie254 <tabbie440@gmail.com>
2022-06-03 02:24:41 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - print a text stdout
  *
- * return: Always 0 (if success)
+ * Return: Always 0 (if success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
