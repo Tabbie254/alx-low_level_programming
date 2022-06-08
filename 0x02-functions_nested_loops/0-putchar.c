c5c7aa89d6e01c2fa809a4f74813451ac7200c5d
Tabbie254 <tabbie440@gmail.com>
2022-06-08 04:22:19 -0700

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Tabbie Muiso
+ *
+ */
+
+#include "putchar.c"
+
+/**
+ * main - prints "Putchar" followed by new line
+ *
+ * Return : 0 always when successful.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
