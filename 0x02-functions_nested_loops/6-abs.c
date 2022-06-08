60c3488bca1ebdfebafd46c3deaa1c86ffa495b9
Tabbie254 <tabbie440@gmail.com>
2022-06-08 12:01:00 -0700

    function that computes absolute values of integers

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,7 +7,7 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the integer to be computed
+ * @i: the integer to be computed
  *
  * Return: the absolute value of the integer.
  */
