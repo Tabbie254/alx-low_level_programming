832ea9c592d71b12d8615c27bde827fc1a5fc9b8
Tabbie254 <tabbie440@gmail.com>
2022-06-08 11:44:53 -0700

    function that prints signs of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,8 +9,7 @@
  * @n: number of which the sign will be printed
  *
  * Return: 1 if the number is > zero
- * 0 if is zero 
- * -1 if the number is less than zero
+ * 0 if is zero  and -1 if the number is less than zero
  */
 int print_sign(int n)
 {
