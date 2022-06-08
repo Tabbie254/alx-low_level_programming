69de19afce47177432198d202eb31b96ab9b6710
Tabbie254 <tabbie440@gmail.com>
2022-06-08 13:45:14 -0700

    list all natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+/*
+ * File: 101-natural.c
+ * Auth: Tabbie Mutiso
+ */
+#include "main.h"
+
+/**
+ * main - lists all natural numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
