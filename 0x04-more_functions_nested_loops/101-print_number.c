8430b0c00f768d14dce3d4916df127fa825ffce7
Tabbie254 <tabbie440@gmail.com>
2022-06-10 14:39:13 -0700

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,25 +10,10 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = n * -1;
+		n = -n;
+		_putchar('-');
 	}
-	else
-	{
-		i = n;
-	}
-
-	j = i;
-	count = 1;
-
-	while (j > 9)
-	{
-		j /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((i / count) % 10) + 48);
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
