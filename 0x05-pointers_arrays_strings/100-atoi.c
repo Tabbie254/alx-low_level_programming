e5ca30168b914b9bffb207efec76e0955d956803
Tabbie254 <tabbie440@gmail.com>
2022-06-13 11:41:53 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 		if (*(s + count) == '-')
 			k *= -1;
 
-		id ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
