0663cf940b0cd56ac23aed4c49c0e50e8495660d
Tabbie254 <tabbie440@gmail.com>
2022-06-13 11:45:33 -0700

    toi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,6 +8,7 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
@@ -30,5 +31,5 @@ int _atoi(char *s)
 		j = j + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
-	return(j * k);
+	return (j * k);
 }
