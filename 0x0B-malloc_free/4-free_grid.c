285fe8c699bd3685326bbeca4f5a8c38bad9b04a
Tabbie254 <tabbie440@gmail.com>
2022-06-29 08:01:54 -0700

    m

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,7 +3,7 @@
 * free_grid - allocates a grid, make space and free space
 * @grid: takes in width of grid
 * @height: height of grid
-* Return: free grid
+* Re free_grid(int **grid, int height)
 */
 voidfree_grid(int **grid, int height)
 {
